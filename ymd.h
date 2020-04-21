6a3727ad0ba72636fe675c0adf2823e4a5058ef5
Ryan Antkowiak <antkowiak@gmail.com>
2020-04-21 06:30:16 -0500

    Added comments

--- ymd.h
+++ ymd.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// Simple representation of a year-month-day data structure
+
 #include <string>
 
 #include "comparable.h"
@@ -12,10 +14,11 @@ protected:
 	int day;
 public:
 
-	YMD() : year(0), month(0), day(0) { }
-	YMD(const int y, const int m, const int d) : year(y), month(m), day(d) { }
-	YMD(const YMD& rhs) : year(rhs.year), month(rhs.month), day(rhs.day) { }
+	YMD() : year(0), month(0), day(0) { } // default constructur
+	YMD(const int y, const int m, const int d) : year(y), month(m), day(d) { } // constructor with initialized data
+	YMD(const YMD& rhs) : year(rhs.year), month(rhs.month), day(rhs.day) { } // copy constructor
 
+	// compare two ymd objects chronologically. uses comparable interface.
 	virtual int compareTo(const YMD& rhs) const
 	{
 		if (year != rhs.year)
@@ -27,6 +30,7 @@ public:
 		return day - rhs.day;
 	}
 
+	// convert to string
 	std::string to_string() const
 	{
 		return std::to_string(year) + "-" + std::to_string(month) + "-" + std::to_string(day);
