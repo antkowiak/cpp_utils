29c88e5023f1f3b6a58bf4ff6b4c779a52df97e0
Ryan Antkowiak <antkowiak@gmail.com>
2020-04-22 10:09:21 -0500

    Abstracted unit test utilities into separate header

--- unit_tests/test_bidirectional_map.h
+++ unit_tests/test_bidirectional_map.h
@@ -5,87 +5,17 @@
 #include <string>
 #include <vector>
 
+
+#include "unit_test_utils.h"
+
 #include "../bidirectional_map.h"
 
 namespace test_bidirectional_map
 {
-	// assert the expression is true
-	static void ASSERT_TRUE(const bool expression, const std::string& description = "")
-	{
-		if (!expression)
-		{
-			std::cout << "ASSERT_TRUE() Failure.";
-			if (description != "")
-				std::cout << " Description: " << description;
-			std::cout << std::endl;
-
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	// assert the expression is false
-	static void ASSERT_FALSE(const bool expression, const std::string& description = "")
-	{
-		if (expression)
-		{
-			std::cout << "ASSERT_FALSE() Failure.";
-			if (description != "")
-				std::cout << " Description: " << description;
-			std::cout << std::endl;
-
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	// call the function and assert it does not throw
-	static void ASSERT_NO_THROW(std::function<void(void)> f, const std::string& description = "")
-	{
-		bool exception_thrown = false;
-
-		try
-		{
-			f();
-		}
-		catch (...)
-		{
-			exception_thrown = true;
-		}
-
-		if (exception_thrown)
-		{
-			std::cout << "ASSERT_NO_THROW() Failure.";
-			if (description != "")
-				std::cout << " Description: " << description;
-			std::cout << std::endl;
-
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	// call the function and assert that it does throw std::out_of_range
-	static void ASSERT_THROWS_OUT_OF_RANGE(std::function<void(void)> f, const std::string& description = "")
-	{
-		bool out_of_range_thrown = false;
-
-		try
-		{
-			f();
-		}
-		catch (std::out_of_range)
-		{
-			out_of_range_thrown = true;
-		}
-
-		if (out_of_range_thrown == false)
-		{
-			std::cout << "ASSERT_THROWS_OUT_OF_RANGE() Failure.";
-			if (description != "")
-				std::cout << " Description: " << description;
-			std::cout << std::endl;
-
-			exit(EXIT_FAILURE);
-		}
-	}
+	using unit_test_utils::ASSERT_TRUE;
+	using unit_test_utils::ASSERT_FALSE;
+	using unit_test_utils::ASSERT_NO_THROW;
+	using unit_test_utils::ASSERT_THROWS_OUT_OF_RANGE;
 
 	struct TestInput
 	{
---
+++ unit_tests/unit_test_utils.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <functional>
+#include <iostream>
+#include <string>
+
+namespace unit_test_utils
+{
+	// assert the expression is true
+	static void ASSERT_TRUE(const bool expression, const std::string& description = "")
+	{
+		if (!expression)
+		{
+			std::cout << "ASSERT_TRUE() Failure.";
+			if (description != "")
+				std::cout << " Description: " << description;
+			std::cout << std::endl;
+
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	// assert the expression is false
+	static void ASSERT_FALSE(const bool expression, const std::string& description = "")
+	{
+		if (expression)
+		{
+			std::cout << "ASSERT_FALSE() Failure.";
+			if (description != "")
+				std::cout << " Description: " << description;
+			std::cout << std::endl;
+
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	// call the function and assert it does not throw
+	static void ASSERT_NO_THROW(std::function<void(void)> f, const std::string& description = "")
+	{
+		bool exception_thrown = false;
+
+		try
+		{
+			f();
+		}
+		catch (...)
+		{
+			exception_thrown = true;
+		}
+
+		if (exception_thrown)
+		{
+			std::cout << "ASSERT_NO_THROW() Failure.";
+			if (description != "")
+				std::cout << " Description: " << description;
+			std::cout << std::endl;
+
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	// call the function and assert that it does throw std::out_of_range
+	static void ASSERT_THROWS_OUT_OF_RANGE(std::function<void(void)> f, const std::string& description = "")
+	{
+		bool out_of_range_thrown = false;
+
+		try
+		{
+			f();
+		}
+		catch (std::out_of_range)
+		{
+			out_of_range_thrown = true;
+		}
+
+		if (out_of_range_thrown == false)
+		{
+			std::cout << "ASSERT_THROWS_OUT_OF_RANGE() Failure.";
+			if (description != "")
+				std::cout << " Description: " << description;
+			std::cout << std::endl;
+
+			exit(EXIT_FAILURE);
+		}
+	}
+}
