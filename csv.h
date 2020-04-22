507319fa42287c0534e3c253d21f2286e1313917
Ryan Antkowiak <antkowiak@gmail.com>
2020-04-22 15:48:08 -0500

    Generalized string to table algo

--- csv.h
+++ csv.h
@@ -7,14 +7,19 @@
 #include "algorithm_rda.h"
 #include "table.h"
 
-static table<std::string> csv_to_table(const std::string& text)
+static table<std::string> string_to_table(const std::string& text, const std::string& row_delim, const std::string& col_delim)
 {
-	std::vector<std::string> lines = split_string_to_vector(text, "\n");
+	std::vector<std::string> lines = split_string_to_vector(text, row_delim);
 
 	std::vector<std::vector<std::string> > cells;
 
 	for (std::string line : lines)
-		cells.push_back(split_string_to_vector(line, ","));
+		cells.push_back(split_string_to_vector(line, col_delim));
 
 	return table<std::string>(cells);
+}
+
+static table<std::string> csv_to_table(const std::string& text)
+{
+	return string_to_table(text, "\n", ",");
 }
