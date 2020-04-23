1493496a6d6fbb34f33fc687f67ac5f7b6816315
Ryan Antkowiak <antkowiak@gmail.com>
2020-04-23 14:11:36 -0500

    Update main

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 
 int main(int argc, char* argv[])
 {
-	//test_bidirectional_map::run_tests();
+	test_bidirectional_map::run_tests();
 	test_fix_message::run_tests();
-	//test_one_to_one_map::run_tests();
-	//test_toolean::run_tests();
+	test_one_to_one_map::run_tests();
+	test_toolean::run_tests();
 
 	return EXIT_SUCCESS;
 }
