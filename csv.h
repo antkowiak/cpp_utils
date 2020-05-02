4c5ebbd7df46bc63cfebd7df0095cd399db1e673
Ryan Antkowiak <antkowiak@gmail.com>
2020-05-02 09:39:17 -0500

    Added namespace qualifier for algorithm

--- csv.h
+++ csv.h
@@ -10,12 +10,13 @@
 static table<std::string> string_to_table(const std::string &text,
                                           const std::string &row_delim,
                                           const std::string &col_delim) {
-  std::vector<std::string> lines = split_string_to_vector(text, row_delim);
+  std::vector<std::string> lines =
+      algorithm_rda::split_string_to_vector(text, row_delim);
 
   std::vector<std::vector<std::string>> cells;
 
   for (std::string line : lines)
-    cells.push_back(split_string_to_vector(line, col_delim));
+    cells.push_back(algorithm_rda::split_string_to_vector(line, col_delim));
 
   return table<std::string>(cells);
 }
