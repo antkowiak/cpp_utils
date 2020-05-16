87b93cc3f8186317f408d7d443ba7005d4e5bb4a
Ryan Antkowiak <antkowiak@gmail.com>
2020-05-16 15:14:14 -0500

    Updated toolean unit tests

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,6 @@
 #include "unit_tests/test_json.h"
 #include "unit_tests/test_one_to_one_map.h"
 #include "unit_tests/test_statemachine.h"
-#include "unit_tests/test_tcp_server.h"
 #include "unit_tests/test_toolean.h"
 #include "unit_tests/test_xml.h"
 #include "unit_tests/test_ymd.h"
@@ -31,8 +30,7 @@ int main()
     rda::test_json().run_tests();
     rda::test_one_to_one_map().run_tests();
     rda::test_statemachine().run_tests();
-    //rda::test_tcp_server::run_tests();
-    //rda::test_toolean::run_tests();
+    rda::test_toolean().run_tests();
     //rda::test_xml::run_tests();
     //rda::test_ymd::run_tests();
 
--- src/unit_tests/test_toolean.h
+++ src/unit_tests/test_toolean.h
@@ -8,9 +8,10 @@
 
 #include <vector>
 
-#include "unit_test_utils.h"
+#include "unit_test_base.h"
 
 #include "../platform_defs.h"
+
 #include "../toolean.h"
 
 PUSH_WARN_DISABLE
@@ -18,145 +19,94 @@ WARN_DISABLE(4100, "-Wunused-parameter")
 
 namespace rda
 {
-    namespace test_toolean
+    class test_toolean : public unit_test_base
     {
-        using unit_test_utils::ASSERT_FALSE;
-        using unit_test_utils::ASSERT_NO_THROW;
-        using unit_test_utils::ASSERT_THROWS;
-        using unit_test_utils::ASSERT_THROWS_OUT_OF_RANGE;
-        using unit_test_utils::ASSERT_TRUE;
-
-        struct TestInput
+    protected:
+        std::string get_test_module_name() const override
         {
-        };
-
-        static void setup(const size_t testNum, TestInput &input)
-        {
-            std::cout << "Running toolean test: " << testNum << std::endl;
+            return "test_toolean";
         }
 
-        static void teardown(const size_t testNum, TestInput &input)
+        void create_tests() override
         {
+            add_test("assignment", [](std::shared_ptr<unit_test_input_base> input) {
+                toolean t;
+                ASSERT_TRUE(t == false);
+
+                t = true;
+                ASSERT_TRUE(t == true);
+
+                t = other;
+                ASSERT_TRUE(t == other);
+            });
+
+            add_test("pre-increment", [](std::shared_ptr<unit_test_input_base> input) {
+                toolean t = false;
+                ASSERT_TRUE(t == false);
+
+                ++t;
+                ASSERT_TRUE(t == true);
+
+                ++t;
+                ASSERT_TRUE(t == other);
+
+                ++t;
+                ASSERT_TRUE(t == false);
+            });
+
+            add_test("post-increment", [](std::shared_ptr<unit_test_input_base> input) {
+                toolean t = false;
+                ASSERT_TRUE(t == false);
+
+                t++;
+                ASSERT_TRUE(t == true);
+
+                t++;
+                ASSERT_TRUE(t == other);
+
+                t++;
+                ASSERT_TRUE(t == false);
+            });
+
+            add_test("post-decrement", [](std::shared_ptr<unit_test_input_base> input) {
+                toolean t = false;
+                ASSERT_TRUE(t == false);
+
+                t--;
+                ASSERT_TRUE(t == other);
+
+                t--;
+                ASSERT_TRUE(t == true);
+
+                t--;
+                ASSERT_TRUE(t == false);
+            });
+
+            add_test("pre-decrement", [](std::shared_ptr<unit_test_input_base> input) {
+                toolean t = false;
+                ASSERT_TRUE(t == false);
+
+                --t;
+                ASSERT_TRUE(t == other);
+
+                --t;
+                ASSERT_TRUE(t == true);
+
+                --t;
+                ASSERT_TRUE(t == false);
+            });
+
+            add_test("friend equality and inequality operators", [](std::shared_ptr<unit_test_input_base> input) {
+                toolean t = false;
+
+                ASSERT_TRUE(false == t);
+                ASSERT_TRUE(true != t);
+                ASSERT_TRUE(other != t);
+            });
         }
 
-        //////////////////////////////////////////////////////////////////////////////////
+    }; // class test_toolean
 
-        static void test_000(const size_t testNum, TestInput &input)
-        {
-            toolean t;
-            ASSERT_TRUE(t == false);
-
-            t = true;
-            ASSERT_TRUE(t == true);
-
-            t = other;
-            ASSERT_TRUE(t == other);
-        }
-
-        static void test_001(const size_t testNum, TestInput &input)
-        {
-            // Removed test case -- No longer expose API to set via a 'char'
-
-            // ASSERT_THROWS([]() { toolean t = 'a'; });
-            // ASSERT_THROWS([]() { toolean t('b'); });
-        }
-
-        static void test_002(const size_t testNum, TestInput &input)
-        {
-            toolean t = false;
-            ASSERT_TRUE(t == false);
-
-            ++t;
-            ASSERT_TRUE(t == true);
-
-            ++t;
-            ASSERT_TRUE(t == other);
-
-            ++t;
-            ASSERT_TRUE(t == false);
-        }
-
-        static void test_003(const size_t testNum, TestInput &input)
-        {
-            toolean t = false;
-            ASSERT_TRUE(t == false);
-
-            t++;
-            ASSERT_TRUE(t == true);
-
-            t++;
-            ASSERT_TRUE(t == other);
-
-            t++;
-            ASSERT_TRUE(t == false);
-        }
-
-        static void test_004(const size_t testNum, TestInput &input)
-        {
-            toolean t = false;
-            ASSERT_TRUE(t == false);
-
-            t--;
-            ASSERT_TRUE(t == other);
-
-            t--;
-            ASSERT_TRUE(t == true);
-
-            t--;
-            ASSERT_TRUE(t == false);
-        }
-
-        static void test_005(const size_t testNum, TestInput &input)
-        {
-            toolean t = false;
-            ASSERT_TRUE(t == false);
-
-            --t;
-            ASSERT_TRUE(t == other);
-
-            --t;
-            ASSERT_TRUE(t == true);
-
-            --t;
-            ASSERT_TRUE(t == false);
-        }
-
-        static void test_006(const size_t testNum, TestInput &input)
-        {
-            // test out friend operators
-
-            toolean t = false;
-
-            ASSERT_TRUE(false == t);
-            ASSERT_TRUE(true != t);
-            ASSERT_TRUE(other != t);
-        }
-
-        static void run_tests()
-        {
-            // vector to hold functions to unit tests
-            std::vector<std::function<void(const size_t, TestInput &)>> test_vec;
-
-            // add all unit tests to the vector
-            test_vec.emplace_back(test_000);
-            test_vec.emplace_back(test_001);
-            test_vec.emplace_back(test_002);
-            test_vec.emplace_back(test_003);
-            test_vec.emplace_back(test_004);
-            test_vec.emplace_back(test_005);
-            test_vec.emplace_back(test_006);
-
-            // run each unit test
-            for (size_t i = 0; i < test_vec.size(); ++i)
-            {
-                TestInput input;
-                setup(i, input);
-                test_vec[i](i, input);
-                teardown(i, input);
-            }
-        }
-
-    } // namespace test_toolean
 } // namespace rda
 
 POP_WARN_DISABLE
