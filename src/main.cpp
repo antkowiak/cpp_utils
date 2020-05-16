939d05af38c9a0c2ec56faefb1b23cbeb0a2af9f
Ryan Antkowiak <antkowiak@gmail.com>
2020-05-16 15:07:08 -0500

    Updated unit tests for statemachine

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ int main()
     rda::test_fix_message().run_tests();
     rda::test_json().run_tests();
     rda::test_one_to_one_map().run_tests();
-    //rda::test_statemachine::run_tests();
+    rda::test_statemachine().run_tests();
     //rda::test_tcp_server::run_tests();
     //rda::test_toolean::run_tests();
     //rda::test_xml::run_tests();
--- src/unit_tests/test_statemachine.h
+++ src/unit_tests/test_statemachine.h
@@ -11,9 +11,10 @@
 #include <string>
 #include <vector>
 
-#include "unit_test_utils.h"
+#include "unit_test_base.h"
 
 #include "../platform_defs.h"
+
 #include "../statemachine.h"
 
 PUSH_WARN_DISABLE
@@ -21,168 +22,135 @@ WARN_DISABLE(4100, "-Wunused-parameter")
 
 namespace rda
 {
-    namespace test_statemachine
+    class test_statemachine : public unit_test_base
     {
-        using unit_test_utils::ASSERT_FALSE;
-        using unit_test_utils::ASSERT_NO_THROW;
-        using unit_test_utils::ASSERT_THROWS;
-        using unit_test_utils::ASSERT_THROWS_OUT_OF_RANGE;
-        using unit_test_utils::ASSERT_TRUE;
-
-        struct TestInput
+    protected:
+        std::string get_test_module_name() const override
         {
-        };
-
-        static void setup(const size_t testNum, TestInput &input)
-        {
-            std::cout << "Running statemachine test: " << testNum
-                      << std::endl;
+            return "test_statemachine";
         }
 
-        static void teardown(const size_t testNum, TestInput &input)
+        void create_tests() override
         {
+            add_test("traffic light", [](std::shared_ptr<unit_test_input_base> input) {
+                statemachine<std::string, std::string> sm("GREEN");
+
+                sm.add_state("GREEN",
+                             [](const std::string &,
+                                const std::string &,
+                                const std::string &) {
+                                 // std::cout << "in state: GREEN" << std::endl;
+                             });
+
+                sm.add_state("YELLOW",
+                             [](const std::string &,
+                                const std::string &,
+                                const std::string &) {
+                                 // std::cout << "in state: YELLOW" << std::endl;
+                             });
+
+                sm.add_state("RED",
+                             [](const std::string &,
+                                const std::string &,
+                                const std::string &) {
+                                 // std::cout << "in state: RED" << std::endl;
+                             });
+
+                sm.add_transition("GREEN", "CHANGE", "YELLOW",
+                                  [](const std::string &,
+                                     const std::string &,
+                                     const std::string &) {
+                                      // std::cout << "transition GREEN CHANGE YELLOW" << std::endl;
+                                  });
+
+                sm.add_transition("YELLOW", "CHANGE", "RED",
+                                  [](const std::string &,
+                                     const std::string &,
+                                     const std::string &) {
+                                      // std::cout << "transition YELLOW CHANGE RED" << std::endl;
+                                  });
+
+                sm.add_transition("RED", "CHANGE", "GREEN",
+                                  [](const std::string &,
+                                     const std::string &,
+                                     const std::string &) {
+                                      // std::cout << "transition RED CHANGE GREEN" << std::endl;
+                                  });
+
+                sm.push_event("CHANGE");
+                sm.run();
+                ASSERT_TRUE(sm.get_current_state() == "YELLOW");
+                sm.push_event("CHANGE");
+                sm.run();
+                ASSERT_TRUE(sm.get_current_state() == "RED");
+                sm.push_event("CHANGE");
+                sm.run();
+                ASSERT_TRUE(sm.get_current_state() == "GREEN");
+                sm.push_event("CHANGE");
+                sm.run();
+                ASSERT_TRUE(sm.get_current_state() == "YELLOW");
+                sm.push_event("CHANGE");
+                sm.run();
+                ASSERT_TRUE(sm.get_current_state() == "RED");
+                sm.push_event("CHANGE");
+                sm.run();
+                ASSERT_TRUE(sm.get_current_state() == "GREEN");
+                sm.push_event("CHANGE");
+                sm.run();
+                ASSERT_TRUE(sm.get_current_state() == "YELLOW");
+            });
+
+            add_test("multiple different transitions", [](std::shared_ptr<unit_test_input_base> input) {
+                statemachine<int, std::string> sm(1);
+
+                sm.add_state(1);
+                sm.add_state(2);
+                sm.add_state(3);
+                sm.add_state(4);
+
+                sm.add_transition(1, "1 to 2", 2);
+                sm.add_transition(1, "1 to 3", 3);
+
+                sm.add_transition(2, "2 to 4", 4);
+                sm.add_transition(2, "2 to 3", 3);
+                sm.add_transition(2, "2 to 1", 1);
+
+                sm.add_transition(3, "3 to 1", 1);
+                sm.add_transition(3, "3 to 4", 4);
+
+                sm.add_transition(4, "4 to 2", 2);
+                sm.add_transition(4, "4 to 1", 1);
+
+                ASSERT_TRUE(sm.get_current_state() == 1);
+
+                sm.push_event("1 to 3");
+                sm.run_one_event();
+                ASSERT_TRUE(sm.get_current_state() == 3);
+
+                bool unhandled_cb_called = false;
+                sm.set_unhandled_event_callback(
+                    [&unhandled_cb_called](const int &state, const std::string &trans) {
+                        unhandled_cb_called = true;
+                        // std::cout << "Unhandler callback from state: " << " transition: " << trans << std::endl;
+                    });
+
+                sm.push_event("3 to 0");
+                sm.run_one_event();
+                ASSERT_TRUE(sm.get_current_state() == 3);
+                ASSERT_TRUE(unhandled_cb_called);
+
+                sm.push_event("3 to 4");
+                sm.run_one_event();
+                ASSERT_TRUE(sm.get_current_state() == 4);
+
+                sm.push_event("4 to 2");
+                sm.run_one_event();
+                ASSERT_TRUE(sm.get_current_state() == 2);
+            });
         }
 
-        //////////////////////////////////////////////////////////////////////////////////
+    }; // class test_statemachine
 
-        static void test_000(const size_t testNum, TestInput &input)
-        {
-            statemachine<std::string, std::string> sm("GREEN");
-
-            sm.add_state("GREEN",
-                         [](const std::string &,
-                            const std::string &,
-                            const std::string &) {
-                             std::cout << "in state: GREEN" << std::endl;
-                         });
-
-            sm.add_state("YELLOW",
-                         [](const std::string &,
-                            const std::string &,
-                            const std::string &) {
-                             std::cout << "in state: YELLOW" << std::endl;
-                         });
-
-            sm.add_state("RED",
-                         [](const std::string &,
-                            const std::string &,
-                            const std::string &) {
-                             std::cout << "in state: RED" << std::endl;
-                         });
-
-            sm.add_transition("GREEN", "CHANGE", "YELLOW",
-                              [](const std::string &,
-                                 const std::string &,
-                                 const std::string &) {
-                                  std::cout << "transition GREEN CHANGE YELLOW" << std::endl;
-                              });
-
-            sm.add_transition("YELLOW", "CHANGE", "RED",
-                              [](const std::string &,
-                                 const std::string &,
-                                 const std::string &) {
-                                  std::cout << "transition YELLOW CHANGE RED" << std::endl;
-                              });
-
-            sm.add_transition("RED", "CHANGE", "GREEN",
-                              [](const std::string &,
-                                 const std::string &,
-                                 const std::string &) {
-                                  std::cout << "transition RED CHANGE GREEN" << std::endl;
-                              });
-
-            sm.push_event("CHANGE");
-            sm.run();
-            ASSERT_TRUE(sm.get_current_state() == "YELLOW");
-            sm.push_event("CHANGE");
-            sm.run();
-            ASSERT_TRUE(sm.get_current_state() == "RED");
-            sm.push_event("CHANGE");
-            sm.run();
-            ASSERT_TRUE(sm.get_current_state() == "GREEN");
-            sm.push_event("CHANGE");
-            sm.run();
-            ASSERT_TRUE(sm.get_current_state() == "YELLOW");
-            sm.push_event("CHANGE");
-            sm.run();
-            ASSERT_TRUE(sm.get_current_state() == "RED");
-            sm.push_event("CHANGE");
-            sm.run();
-            ASSERT_TRUE(sm.get_current_state() == "GREEN");
-            sm.push_event("CHANGE");
-            sm.run();
-            ASSERT_TRUE(sm.get_current_state() == "YELLOW");
-        }
-
-        static void test_001(const size_t testNum, TestInput &input)
-        {
-            statemachine<int, std::string> sm(1);
-
-            sm.add_state(1);
-            sm.add_state(2);
-            sm.add_state(3);
-            sm.add_state(4);
-
-            sm.add_transition(1, "1 to 2", 2);
-            sm.add_transition(1, "1 to 3", 3);
-
-            sm.add_transition(2, "2 to 4", 4);
-            sm.add_transition(2, "2 to 3", 3);
-            sm.add_transition(2, "2 to 1", 1);
-
-            sm.add_transition(3, "3 to 1", 1);
-            sm.add_transition(3, "3 to 4", 4);
-
-            sm.add_transition(4, "4 to 2", 2);
-            sm.add_transition(4, "4 to 1", 1);
-
-            ASSERT_TRUE(sm.get_current_state() == 1);
-
-            sm.push_event("1 to 3");
-            sm.run_one_event();
-            ASSERT_TRUE(sm.get_current_state() == 3);
-
-            bool unhandled_cb_called = false;
-            sm.set_unhandled_event_callback(
-                [&unhandled_cb_called](const int &state, const std::string &trans) {
-                    unhandled_cb_called = true;
-                    std::cout << "Unhandler callback from state: "
-                              << " transition: " << trans << std::endl;
-                });
-
-            sm.push_event("3 to 0");
-            sm.run_one_event();
-            ASSERT_TRUE(sm.get_current_state() == 3);
-            ASSERT_TRUE(unhandled_cb_called);
-
-            sm.push_event("3 to 4");
-            sm.run_one_event();
-            ASSERT_TRUE(sm.get_current_state() == 4);
-
-            sm.push_event("4 to 2");
-            sm.run_one_event();
-            ASSERT_TRUE(sm.get_current_state() == 2);
-        }
-
-        static void run_tests()
-        {
-            // vector to hold functions to unit tests
-            std::vector<std::function<void(const size_t, TestInput &)>> test_vec;
-
-            // add all unit tests to the vector
-            test_vec.emplace_back(test_000);
-            test_vec.emplace_back(test_001);
-
-            // run each unit test
-            for (size_t i = 0; i < test_vec.size(); ++i)
-            {
-                TestInput input;
-                setup(i, input);
-                test_vec[i](i, input);
-                teardown(i, input);
-            }
-        }
-    } // namespace test_statemachine
 } // namespace rda
 
 POP_WARN_DISABLE
