d1a1b85231c7f01e9c4555884748504b8b01da2f
Ryan Antkowiak <antkowiak@gmail.com>
2020-06-15 09:24:59 -0500

    Updated lifetime

--- src/lifetime.h
+++ src/lifetime.h
@@ -14,83 +14,39 @@ namespace rda
 {
     class lifetime
     {
-    private:
-        std::string name;
-        size_t id;
-
-        static size_t counter;
-
-        static std::string generate_string(const std::string &reason,
-                                           const lifetime &obj,
-                                           const lifetime *const fromPtr = nullptr)
-        {
-            std::stringstream ss;
-            ss << "[";
-            ss.width(17);
-            ss << reason << "] [ID: ";
-            ss.width(3);
-            ss << obj.id << "] [NAME: ";
-            ss.width(8);
-            ss << std::left << obj.name << std::right << "] [ADDR: 0x" << (&obj) << "]";
-
-            if (fromPtr != nullptr)
-            {
-                ss << " [FROM: ";
-                ss.width(3);
-                ss << fromPtr->id << "]";
-            }
-
-            return ss.str();
-        }
-
     public:
         lifetime()
         {
-            name = "";
-            id = ++counter;
-            std::cout << generate_string("DEFAULT_CONSTRUCT", *this) << std::endl;
-        }
-
-        lifetime(const std::string &n)
-        {
-            name = n;
-            id = ++counter;
-            std::cout << generate_string("CONSTRUCT", *this) << std::endl;
-        }
-
-        lifetime(const lifetime &rhs)
-        {
-            name = rhs.name;
-            id = ++counter;
-            std::cout << generate_string("COPY_CONSTRUCT", *this, &rhs) << std::endl;
-        }
-
-        lifetime(const lifetime&& rhs) noexcept
-        {
-            name = rhs.name;
-            id = ++counter;
-            std::cout << generate_string("MOVE_CONSTRUCT", *this, &rhs) << std::endl;
+            std::cout << "[" << this << "] - default construct - lifetime()" << std::endl;
         }
 
         ~lifetime()
         {
-            std::cout << generate_string("DESTRUCT", *this) << std::endl;
+            std::cout << "[" << this << "] - destruct          - ~lifetime()" << std::endl;
         }
 
-        lifetime &operator=(const lifetime &rhs)
+        lifetime(const lifetime&)
         {
-            std::cout << generate_string("ASSIGN", *this, &rhs) << std::endl;
-            name = rhs.name;
+            std::cout << "[" << this << "] - copy construct    - lifetime(const lifetime&)" << std::endl;
+        }
+
+        lifetime(lifetime&&) noexcept
+        {
+            std::cout << "[" << this << "] - move construct    - lifetime(lifetime&&) noexcept" << std::endl;
+        }
+
+        lifetime& operator = (const lifetime &)
+        {
+            std::cout << "[" << this << "] - assign            - lifetime& operator = (const lifetime &)" << std::endl;
             return *this;
         }
 
-        friend std::ostream &operator<<(std::ostream &os, const lifetime &rhs)
+        lifetime& operator = (lifetime&&) noexcept
         {
-            os << generate_string("PRINT", rhs);
-            return os;
+            std::cout << "[" << this << "] - move assign       - lifetime& operator = (lifetime&&) noexcept" << std::endl;
+            return *this;
         }
-    }; // class lifetime
 
-    size_t lifetime::counter = 0;
+    }; // class lifetime
 
 } // namespace rda
